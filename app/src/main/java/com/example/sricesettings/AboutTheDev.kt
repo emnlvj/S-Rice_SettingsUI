d456d7e5fbb22d192358dafc6dc5098640bb2e54
sellwynn <emmanueljacabav@gmail.com>
2025-05-18 23:52:48 +0800

    Settings UI
    Settings UI

---
+++ app/src/main/java/com/example/sricesettings/AboutTheDev.kt
@@ -0,0 +1,17 @@
+package com.example.sricesettings
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AboutTheDev : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_about_the_dev)
+
+    }
+}
